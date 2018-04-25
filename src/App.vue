9e9c9a283259eda3ecc767f7eff9e2115b9fc702
yndxz <2465969877@qq.com>
2018-04-25 14:56:27 +0800

    基于vue-2.0的图片滚动组件

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <scroll-picture></scroll-picture>
+</template>
+
+<script>
+    import ScrollPicture from './components/slide'
+    export default {
+        name: "app",
+        components:{
+            'scroll-picture':ScrollPicture
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
-import Slide from './slide.vue';
+import App from './App.vue';
 new Vue({
 	el:'#app',
-	render:h=>h(Slide)
-})
+	render:h=>h(App)
+});
